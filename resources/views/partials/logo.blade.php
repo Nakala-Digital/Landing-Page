f44f9e7d361ff8efae3938fddd1d6613a7f07c83
git push <raulmahya11@gmail.com>
2026-06-02 21:56:53 +0700

    fix: update logo rendering logic to support PNG fallback

--- resources/views/partials/logo.blade.php
+++ resources/views/partials/logo.blade.php
@@ -1,9 +1,16 @@
 @php
-    $logoPath = public_path($site['brand']['logo']);
-    $logoSvg = file_exists($logoPath) ? file_get_contents($logoPath) : null;
+    $svgPath = public_path($site['brand']['logo']);
+    $pngPath = public_path($site['brand']['logo_png']);
+    $logoSvg = file_exists($svgPath) ? file_get_contents($svgPath) : null;
     $logoSvg = $logoSvg ? preg_replace('/<\?xml.*?\?>\s*/', '', $logoSvg) : null;
 @endphp
 
 <span class="inline-logo" aria-label="{{ $site['brand']['name'] }} logo">
-    {!! $logoSvg ?: e($site['brand']['name']) !!}
+    @if (file_exists($pngPath))
+        <img src="{{ asset($site['brand']['logo_png']) }}" alt="{{ $site['brand']['name'] }}" style="width:100%;height:100%;object-fit:contain;display:block;" />
+    @elseif ($logoSvg)
+        {!! $logoSvg !!}
+    @else
+        {{ $site['brand']['name'] }}
+    @endif
 </span>
